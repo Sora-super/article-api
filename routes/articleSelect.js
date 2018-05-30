235149eccd6ec0f3619e89f71295c9c18eff76bc
zhaoxing.xue <zhaoxing.xue@zhaopin.com.cn>
2018-05-30 17:29:59 +0800

    dev

--- routes/articleSelect.js
+++ routes/articleSelect.js
@@ -10,34 +10,47 @@ const pool = mysql.createPool(dbConfig.mysql)
 
 // 响应一个JSON数据
 var responseJSON = function (res, ret) {
-  if(typeof ret === 'undefined') { 
-       res.json({     code:'-200',     msg: '操作失败'   
-     }); 
- } else { 
-   res.json(ret); 
-}};
-// 添加用户
-router.get('/selectArticle', function(req, res, next){
+  if (typeof ret === 'undefined') {
+    res.json({
+      code: '-200',
+      msg: '操作失败'
+    });
+  } else {
+    res.json(ret);
+  }
+};
+// 获取文章
+router.get('/selectArticle', function (req, res, next) {
   // 从连接池获取连接 
-  pool.getConnection(function(err, connection) { 
-  // 获取前台页面传过来的参数  
-  // var param = req.query || req.params;   
-  // 建立连接 增加一个用户信息 
-  connection.query(articleSQL.queryAll,function(err, result) {
-      if(result) {
-            result = {   
-                    code: 200,   
-                    msg: result
-            };  
-      }     
-        
-    // 以json形式，把操作结果返回给前台页面     
-      responseJSON(res, result);   
+  pool.getConnection(function (err, connection) {
+    // 获取前台页面传过来的参数  
+    // var param = req.query || req.params;   
+    // 建立连接 增加一个用户信息 
+    try {
+      connection.query(articleSQL.queryAll, function (err, result) {
+        if (result) {
+          result = {
+            code: 200,
+            msg: result
+          };
+        }
 
-    // 释放连接  
-    connection.release();  
+        // 以json形式，把操作结果返回给前台页面     
+        responseJSON(res, result);
+
+        // 释放连接  
+        connection.release();
 
       });
+    } catch (e) {
+      console.log(e)
+      result = {
+        code: 500,
+        msg: '内部错误'
+      };
+      responseJSON(res, result);
+    }
+
   });
 });
 
